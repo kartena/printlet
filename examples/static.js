15e6f042329248b0bfa2eafabba98edefdc8515b
Christopher Fredén <c.freden@gmail.com>
2015-02-03 14:09:51 +0100

    Added text marker styling in the static example

--- examples/static.js
+++ examples/static.js
@@ -2,16 +2,20 @@ var fs = require('fs'),
     printlet = require('../lib/printlet'),
     tileJson = require('./osm.json');
 
+// Create a printlet instance from a TileJSON
 printlet(tileJson)({
+  // Specify the image options
   width: 800,
   height: 600,
   zoom: 12,
   lng: 11.95,
   lat: 57.7,
   format: 'png',
+  // Add the features to be drawn
   geojson: {
     "type": "FeatureCollection",
     "features": [
+      // Draw a semi-transparent line
       {
         "type": "Feature",
         "geometry": {
@@ -20,8 +24,25 @@ printlet(tileJson)({
         },
         "properties": {
           "style": {
-            "strokeStyle": "rgb(200, 0, 0, 0.6)",
+            "strokeStyle": "rgba(200, 0, 0, 0.6)",
             "lineWidth": "5"
+          }
+        }
+      },
+      // Draw a point represented by a text marker
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [11.95, 57.7]
+        },
+        "properties": {
+          "style": {
+            "font": "32px serif",
+            "fillStyle": "rgb(20, 20, 120)",
+            "marker": {
+              "text": "Hello World"
+            }
           }
         }
       }
@@ -29,5 +50,6 @@ printlet(tileJson)({
   }
 }, function(err, data) {
   if (err != null) throw new Error(err + '\n' + err.stack);
+  // Get the image data as a stream and write save it to a file
   data.stream.pipe(fs.createWriteStream('image.png'));
 });
